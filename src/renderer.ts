dea7b9302e16aba663dad5617528ddf9c6690663
Dan Gizzi <gizzidan@me.com>
2022-08-27 16:26:26 -0400

    Styling

--- src/renderer.ts
+++ src/renderer.ts
@@ -13,32 +13,32 @@ import { Feeds, JSONValue } from "./@types/bubo";
  * Global filters for my Nunjucks templates
  */
 env.addFilter("formatDate", function (dateString): string {
-  const date: Date = new Date(parseInt(dateString));
-  return !isNaN(date.getTime()) ? date.toLocaleDateString() : dateString;
+	const date: Date = new Date(parseInt(dateString));
+	return !isNaN(date.getTime()) ? date.toLocaleDateString() : dateString;
 });
 
-env.addGlobal("now", new Date().toUTCString());
+env.addGlobal("now", new Date().toLocaleString());
 
 // load the template
 const template: string = (
-  await readFile(new URL("../config/template.html", import.meta.url))
+	await readFile(new URL("../config/template.html", import.meta.url))
 ).toString();
 
 // generate the static HTML output from our template renderer
 const render = ({
-  data,
-  errors,
-  info
+	data,
+	errors,
+	info,
 }: {
-  data: Feeds;
-  errors: unknown[];
-  info?: JSONValue;
+	data: Feeds;
+	errors: unknown[];
+	info?: JSONValue;
 }) => {
-  return env.renderString(template, {
-    data,
-    errors,
-    info
-  });
+	return env.renderString(template, {
+		data,
+		errors,
+		info,
+	});
 };
 
 export { render };
